187fd6b1bc2572200ac4d06dd56c72f42845f87c
dbasargin <dennisbasargin@hotmail.com>
2017-10-03 18:52:56 -0700

    This is my Initial commit to repository

---
+++ FileReaderForRusselInvestments/FileHelperClass.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderForRusselInvestments
+{
+    /// <summary>
+    /// This is the file helper class and contains methods for 
+    /// creating and manipulating files
+    /// </summary>
+    public static class FileHelperClass
+    {
+        /// <summary>
+        /// This method creates the initial .txt file if it does not already exist;
+        /// if it does exist the method clears the file from any text.
+        /// </summary>
+        public static bool CreateInitialTextFile(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    FileStream fStream = File.Create(path);
+                    fStream.Close();
+                }
+                else
+                {
+                    ClearFileContent(path);
+                }
+                return true;
+            }
+            //display error 
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error occurred creating file: Exception type of " + ex.GetType() );
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Creates a path for new .txt file
+        /// </summary>
+        /// <returns>fully qualified path as a string</returns>
+        public static string CreatePath()
+        {
+            string txtFileName = "DennisBProject.txt";
+            string path = @"c\";
+            var folder = Path.Combine(path, "DennisBFolder");
+            Directory.CreateDirectory(folder);
+            return path + txtFileName;
+        }
+
+        /// <summary>
+        /// Adds a line to a .txt file
+        /// </summary>
+        /// <param name="path">path of the file</param>
+        /// <param name="line">the line you want added to file</param>
+        public static void AppendFile(string path, string line)
+        {
+            StreamWriter strmWriter = null;
+
+            try
+            {
+                using (strmWriter = (File.Exists(path)) ? File.AppendText(path) : File.CreateText(path))
+                {
+                    strmWriter.WriteLine(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.GetType() + "::Error adding line to: " + path);
+            }
+            finally
+            {
+                if(strmWriter != null)
+                {
+                    strmWriter.Dispose();
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// overwrites existing file with empty file
+        /// </summary>
+        /// <param name="path">path to file</param>
+        public static void ClearFileContent(string path)
+        {
+            var fl = File.Create(path);
+            fl.Close();
+        }
+
+        /// <summary>
+        /// gets all lines from file, sorts by line length and displays longest line, 
+        /// longest lines with the same length or nothing if file is empty
+        /// </summary>
+        /// <param name="path">path to file</param>
+        public static void DisplayLongestLineInFile(string path)
+        {
+            //initialize stream
+            StreamReader strmReader = null;
+
+            //try accessing and reading file
+            try
+            {
+                using (strmReader = new StreamReader(path))
+                {
+                    List<String> textFileLines = new List<string>();
+
+                    while (!strmReader.EndOfStream)
+                    {
+                        string newLine = strmReader.ReadLine();
+                        textFileLines.Add(newLine);
+                    }
+
+                    //Orders Lines by string length with LINQ(insures best practice)
+                    textFileLines = textFileLines.OrderByDescending(x => x.Length).ToList();
+
+                    //return without displaying lines if file is empty
+                    if (textFileLines.Count() == 0)
+                    {
+                        strmReader.Dispose();
+                        return;
+                    }
+                    // display lines with longest string length
+                    else
+                    {
+                        for (int i = 0; i < textFileLines.Count(); i++)
+                        {
+                            if (i == 0 || textFileLines[i].Length == textFileLines[0].Length)
+                            {
+                                Console.WriteLine(textFileLines[i]);
+                            }
+                        }
+
+                        AddEmptyLine();
+                    }
+                }
+            }
+            //add error message if access to file fails
+            catch
+            {
+                Console.WriteLine("error accessing path: " + path);
+            }
+            //Dispose stream
+            finally
+            {
+                if (strmReader != null)
+                {
+                    strmReader.Dispose();
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// allows user to add lines to a .txt file.
+        /// </summary>
+        /// <param name="path">path to file</param>
+        public static void CreateFileContent(string path)
+        {
+            //message to user
+            string displayAddContentInstructions = "Type a line of text and click ENTER to add a line of text to your file.";
+            Console.WriteLine(displayAddContentInstructions);
+
+            bool addAnotherLine = true;
+
+            //allow user to add mulitple lines
+            while (addAnotherLine)
+            {
+                string input = Console.ReadLine();
+                FileHelperClass.AppendFile(path, input);
+                AddEmptyLine();
+
+
+                //if user response is invalid: retry
+                bool invalidInput = true;
+
+                while (invalidInput)
+                {
+                    Console.WriteLine("Add another Line?(Y/N)");
+
+                    //read user input and convert it to lower
+                    string continueInput = Console.ReadLine().ToLower();
+                    AddEmptyLine();
+
+                    if (continueInput == "y")
+                    {
+                        addAnotherLine = true;
+                        invalidInput = false;
+                        Console.WriteLine(displayAddContentInstructions);
+                    }
+                    else if (continueInput == "n")
+                    {
+                        addAnotherLine = false;
+                        invalidInput = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine(continueInput + " is invalid input: RETRY");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds empty line to console window
+        /// </summary>
+        public static void AddEmptyLine()
+        {
+            Console.WriteLine();
+        }
+    }
+}
--- FileReaderForRusselInvestments/Program.cs
+++ FileReaderForRusselInvestments/Program.cs
@@ -1,15 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FileReaderForRusselInvestments
 {
     class Program
     {
+                
+        /// <summary>
+        ///  Write a program that reads lines from standard input and, upon end of file, writes the longest line to standard output. 
+        ///  If there are ties for the longest line, the program writes out all the lines that tie. If there is no input, the program should produce no output.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            DisplayProgramHeader();
+            string path = FileHelperClass.CreatePath();
+            bool fileCreated =  FileHelperClass.CreateInitialTextFile(path);
+
+            //initialize menuOption to get started
+            string menuOption = "0";
+
+            if (fileCreated)
+            { 
+                //Continue program until user selects option 3)Quit
+                while (menuOption != "3")
+                {
+                    DisplayMenuOptions();
+                    menuOption =  SelectMenuOption();
+
+                    if(menuOption == "1")
+                    {
+                        FileHelperClass.CreateFileContent(path);
+                    }
+                    if (menuOption == "2")
+                    {
+                        FileHelperClass.DisplayLongestLineInFile(path);
+                    }
+                }
+            }
+
         }
+
+
+        /// <summary>
+        /// Reads and returns users input for selected menu options
+        /// </summary>
+        /// <returns></returns>
+        private static string SelectMenuOption()
+        {
+            string input = "0";
+
+            try
+            {
+                input = Console.ReadLine();
+
+                if (input != "1" && input != "2" && input != "3")
+                {
+                    Console.WriteLine("Invalid Menu Option: try again");
+                }
+            }
+            //if user response is invalid: retry
+            catch
+            {
+                Console.WriteLine("Invalid Menu Option: try again");
+            }
+
+            AddEmptyLine();
+
+            return input;
+
+        }
+
+
+        /// <summary>
+        /// This is the program header displayed at the beggining of program
+        /// </summary>
+        private static void DisplayProgramHeader()
+        {
+            Console.WriteLine("Welcome! This program will read the longest lines of a .txt file.");
+            Console.WriteLine("If there are ties for the longest line, the program writes out all the lines that tie.");
+            Console.WriteLine("If there is no input, the program will produce no output.");
+            AddEmptyLine();
+        }
+
+        /// <summary>
+        /// Displays menu to console window
+        /// </summary>
+        private static void DisplayMenuOptions()
+        {
+            Console.WriteLine("Please type an option number below and press enter:");
+            Console.WriteLine("1) Add content to a file");
+            Console.WriteLine("2) Display file line with longest text");
+            Console.WriteLine("3) Quit");
+        }
+
+        /// <summary>
+        /// Adds empty line to console window:
+        /// Makes program easier to read
+        /// </summary>
+        public static void AddEmptyLine()
+        {
+            Console.WriteLine();
+        }
+
     }
 }
